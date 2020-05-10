989775eeb194259a2cb7b186e8763dd96913f9cc
KelvinTao <xianming_tao@163.com>
2020-05-10 11:47:21 +0800

    Create outputSum.m
    Summary for searching result.

---
+++ outputSum.m
@@ -0,0 +1,20 @@
+function outSum=outputSum(sumI)
+   row=0;
+   [head,~]=size(sumI);
+   for headN=1:head
+       [col,~]=size(sumI{headN,1});
+        row=row+col;%----------找总行数
+   end
+   outSum=zeros(row,5);rowN=0;
+   for headN=1:head
+       [col,~]=size(sumI{headN,1});
+       for colN=1:col
+           rowN=rowN+1;
+           outSum(rowN,1)=sumI{headN,1}(colN,1);
+           outSum(rowN,2)=sumI{headN,1}(colN,2);
+           outSum(rowN,3)=sumI{headN,2}(colN,1);
+           outSum(rowN,4)=sumI{headN,2}(colN,2);
+           outSum(rowN,5)=sumI{headN,3}(colN,1);
+       end
+   end
+end
